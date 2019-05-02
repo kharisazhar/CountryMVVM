ef0780f76bd6f02c14000147a4ce4460d9848f5b
Kharis Azhar <muhammadkharisazhar149a@gmail.com>
2019-05-02 13:31:48 +0700

    added image country

--- app/src/main/java/com/kharisazhar/country/view/CountryListAdapter.kt
+++ app/src/main/java/com/kharisazhar/country/view/CountryListAdapter.kt
@@ -6,9 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.kharisazhar.country.R
 import com.kharisazhar.country.model.Country
+import com.kharisazhar.country.util.getProgressDrawable
+import com.kharisazhar.country.util.loadImage
 import kotlinx.android.synthetic.main.item_country.view.*
 
-class CountryListAdapter(var countries: ArrayList<Country>): RecyclerView.Adapter<CountryListAdapter.CountryViewHolder>() {
+class CountryListAdapter(var countries: ArrayList<Country>) :
+    RecyclerView.Adapter<CountryListAdapter.CountryViewHolder>() {
 
     fun updateCountries(newCountries: List<Country>) {
         countries.clear()
@@ -17,7 +20,7 @@ class CountryListAdapter(var countries: ArrayList<Country>): RecyclerView.Adapte
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int) = CountryViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.item_country,parent,false)
+        LayoutInflater.from(parent.context).inflate(R.layout.item_country, parent, false)
     )
 
     override fun getItemCount() = countries.size
@@ -26,12 +29,17 @@ class CountryListAdapter(var countries: ArrayList<Country>): RecyclerView.Adapte
         holder.bind(countries[position])
     }
 
-    class CountryViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    class CountryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         private val countryName = view.name
+        private val imageView = view.imageView
+        private val countryCapital = view.capital
+        private val progressDrawable = getProgressDrawable(view.context)
 
         fun bind(country: Country) {
             countryName.text = country.countryName
+            countryCapital.text = country.capital
+            imageView.loadImage(country.flag, progressDrawable)
         }
     }
 }
