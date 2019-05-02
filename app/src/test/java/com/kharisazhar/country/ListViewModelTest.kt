792e065c29ef7237d31c2b39a5e66e05f40638a1
Kharis Azhar <muhammadkharisazhar149a@gmail.com>
2019-05-02 15:15:02 +0700

    unit testing simple

---
+++ app/src/test/java/com/kharisazhar/country/ListViewModelTest.kt
@@ -0,0 +1,77 @@
+package com.kharisazhar.country
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import com.kharisazhar.country.model.Country
+import com.kharisazhar.country.network.CountriesService
+import com.kharisazhar.country.viewmodel.ListViewModel
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.disposables.Disposable
+import io.reactivex.internal.schedulers.ExecutorScheduler
+import io.reactivex.plugins.RxJavaPlugins
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
+
+class ListViewModelTest {
+
+    @get:Rule
+    var rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var countriesService: CountriesService
+
+    @InjectMocks
+    var listViewModel = ListViewModel()
+
+    private var testSingle: Single<List<Country>>? = null
+
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun getCountriesSuccess() {
+        val country = Country("countryName", "capital", "url")
+        val countriesList = arrayListOf(country)
+
+        testSingle = Single.just(countriesList)
+
+        `when`(countriesService.getCountries()).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        assertEquals(1, listViewModel.countries.value?.size)
+        assertEquals(false, listViewModel.countryLoadError.value)
+        assertEquals(false, listViewModel.loading.value)
+    }
+
+    @Before
+    fun setUpRxSchedulers() {
+        val immediate = object : Scheduler() {
+            override fun scheduleDirect(run: Runnable?, delay: Long, unit: TimeUnit?): Disposable {
+                return super.scheduleDirect(run, 0, unit)
+            }
+
+            override fun createWorker(): Worker {
+                return ExecutorScheduler.ExecutorWorker(Executor { it.run() })
+            }
+        }
+
+        RxJavaPlugins.setInitIoSchedulerHandler { scheduler -> immediate }
+        RxJavaPlugins.setInitComputationSchedulerHandler { scheduler -> immediate }
+        RxJavaPlugins.setInitNewThreadSchedulerHandler { scheduler -> immediate }
+        RxJavaPlugins.setInitSingleSchedulerHandler { scheduler -> immediate }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler -> immediate }
+    }
+}
